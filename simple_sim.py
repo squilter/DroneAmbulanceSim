224f5d8f56364dfb133c86452164ed0ca1a6fada
squilter <s.quilter@aol.com>
2015-01-06 10:32:42 -0500

    swap backwards lat/lon

--- simple_sim.py
+++ simple_sim.py
@@ -7,7 +7,7 @@ with open('data/FirehouseData.csv') as firehouse_file:
 	reader = csv.reader(firehouse_file, delimiter=',', quotechar=';')
 	reader.next()#skip the first line with headers
 	for row in reader:
-		firehouses.append((float(row[0]),float(row[1])))
+		firehouses.append((float(row[1]),float(row[0])))
 		
 def find_closest_firehouse(incident_coord):
 	closest_distance = 99999999999
@@ -18,10 +18,13 @@ def find_closest_firehouse(incident_coord):
 			closest_distance=distance
 	return closest_distance
 
-with open('data/incidents_with_latlongs.csv') as csvfile:
+with open('data/incidents_with_latlongs.csv', 'rU') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar=';')
+	reader.next()
 	for row in reader:
-		print(find_closest_firehouse(float(row[15]),float(row[16])))
-
+		try:
+			print(find_closest_firehouse((float(row[15]),float(row[16]))))
+		except ValueError:
+			print(0)
 exit()
 
