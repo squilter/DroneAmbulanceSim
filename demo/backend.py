eba34df0bac51a84ddf2d3a17b298c9e78e3b3e3
squilter <s.quilter@aol.com>
2015-03-14 13:47:47 -0400

    parse latlon

--- demo/backend.py
+++ demo/backend.py
@@ -5,11 +5,18 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 hostName = "localhost"
 hostPort = 80
 
+def handle_latlon(path):
+	(lat_s,lon_s)=path[1:].split(",",1)
+	(lat,lon)=(float(lat_s),float(lon_s))
+	print(lat)
+	print(lon)
+
 class MyServer(BaseHTTPRequestHandler):
 	def do_GET(self):
 		self.send_response(200)
 		self.send_header("Content-type","text/html")
 		self.end_headers()
+		handle_latlon(self.path)
 		self.wfile.write(bytes("<html><body><p>seb</p></body></html>","utf-8"))
 
 myServer = HTTPServer((hostName,hostPort), MyServer)
