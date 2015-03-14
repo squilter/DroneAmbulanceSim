33f3f8f97e7d872931e72c0daac36b322adcadd2
squilter <s.quilter@aol.com>
2015-03-14 16:56:00 -0400

    generate html table

--- demo/backend.py
+++ demo/backend.py
@@ -4,7 +4,7 @@ import csv
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from geopy.distance import vincenty
 
-hostName = "localhost"
+hostName = ""
 hostPort = 80
 
 def find_closest_firehouse(incident_coord):
@@ -17,28 +17,50 @@ def find_closest_firehouse(incident_coord):
         return closest_distance
 
 def convert_meters_to_drone_time(dist):
-        return(5+30+dist/10)
+        return((5+30+dist/10)/60)
 
-def handle_latlon(path):
+def generate_response(path):
+        response = "<H3>Drone Response Time: "
         (lat_s,lon_s)=path[1:].split(",",1)
         (lat,lon)=(float(lat_s),float(lon_s))
-        print(lat)
-        print(lon)
-        print("closest firehouse: ",find_closest_firehouse((lat,lon)))	
-        print("drone time: ",convert_meters_to_drone_time(find_closest_firehouse((lat,lon))))	
+        drone_dist = find_closest_firehouse((lat,lon))	
+        if(drone_dist<3000):
+                response += str(round(convert_meters_to_drone_time(drone_dist),2))	
+                response += "mins."
+        else:
+                response += "N/A"
+        response += "</H3><p />"
+
+        response += "<table><col width=\"80\"><col width=\"80\"><col width=\"80\"><tr><td><b>Vehicle</b></td><td><b>Response<br />Time (min)</b></td><td><b>Date</b></td></tr>"
         for incident in incidents:
+                if(lat-incident[0]>0.002):# if lat/lon are very different, don't bother caltulating actual dist
+                        continue
+                if(lon-incident[1]>0.002):
+                        continue
                 dist=vincenty((lat,lon),(incident[0],incident[1])).meters
                 if(dist<=150):
-                        print("found incident "+ str(dist)+"m away:")
-                        print(incident)
+                        response += "<tr>"
+                        response += "<td>"+str(incident[3])+"</td>"#vehicle type
+                        response += "<td>"+str(round(incident[2],2))+"</td>"#response time
+                        response += "<td>"+str(incident[4])+"</td>"#date
+                        response += "</tr>"
+        response += "</table>"
+        return response
 
 class MyServer(BaseHTTPRequestHandler):
         def do_GET(self):
                 self.send_response(200)
                 self.send_header("Content-type","text/html")
+                self.send_header('Access-Control-Allow-Origin', '*')                
                 self.end_headers()
-                handle_latlon(self.path)
-                self.wfile.write(bytes("<html><body><p>seb</p></body></html>","utf-8"))
+                html_response = bytes(generate_response(self.path),"utf-8")
+                self.wfile.write(html_response)
+        def do_OPTIONS(self):
+                self.send_response(200, "ok")       
+                self.send_header('Access-Control-Allow-Origin', '*')                
+                self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+                self.send_header('Access-Control-Allow-Headers', 'access-control-allow-origin, accept')  
+                self.end_headers()
 
 #preload firehouses
 firehouses=[]
